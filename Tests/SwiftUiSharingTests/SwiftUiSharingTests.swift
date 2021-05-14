f82243469618dc51015c1f19bf13f6891ccd411f
Kevin Mullins <kevin_mullins@appracatappra.com>
2021-05-14 11:52:22 -0500

    Updated Utilities & Unit Tests
    Updated to latest `SwiftletUtilities` and added Unit Tests.

---
+++ Tests/SwiftUiSharingTests/SwiftUiSharingTests.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftUiSharingTests.swift
+//  
+//
+//  Created by Kevin Mullins on 5/14/21.
+//
+
+import XCTest
+import SwiftUI
+@testable import SwiftUiSharing
+
+final class SwiftUiSharingTests: XCTestCase {
+    
+    func testSwiftUiSharing() {
+        #if os(iOS)
+        let text = "Hello World"
+        let item = SharingSheetItem(data: text)
+        XCTAssert(item != nil)
+        #else
+        XCTAssert(true)
+        #endif
+    }
+}
